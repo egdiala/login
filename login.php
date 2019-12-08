a9c6d99072c12c7fdb5503b842acc15f3290b563
egdiala <46764458+egdiala@users.noreply.github.com>
2019-12-09 00:06:49 +0100

    Add files via upload

---
+++ login.php
@@ -0,0 +1,18 @@
+<?php
+
+
+$_POST = json_decode(file_get_contents("php://input"), true);
+// header('Content-Type: application/json');
+$uname = $_POST['username'];
+$pwd = $_POST['password'];
+
+
+
+if (isset($uname) && isset($pwd)) {
+    // based on successful authentication
+    $successMSG = '<p>Login successful.</p>';
+    echo json_encode(['code' => 1, 'msg' => $successMSG]);
+} else {
+    $errorMSG = '<p>Login not successful.</p>';
+    echo json_encode(['code' => 404, 'msg' => $errorMSG]);
+}
