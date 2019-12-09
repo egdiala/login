4cc065d5765b8ea76c84a160947cf57a8f281518
egdiala <46764458+egdiala@users.noreply.github.com>
2019-12-09 10:30:48 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
 
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin");
 header('Content-Type: application/json');
 $_POST = json_decode(file_get_contents("php://input"), true);
 
